d5ea05067d9539f5782c5a079cfbcb3525d62616
Gekon <grzegorz.szustkiewicz@gmail.com>
2022-08-10 20:41:02 +0200

    Zmiana w drugim repo

--- src/main/resources/Main.kt
+++ src/main/resources/Main.kt
@@ -2,4 +2,8 @@ public class Main {
     public static voic main(String[] args) {
         System.out.println("Hello World");
     }
+
+    private void displayText() {
+        System.out.println("Zmiana w drugim repo");
+    }
 }
